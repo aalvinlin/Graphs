febe66d97c9985efe2d994103467dc8896a0c1e3
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-06-09 09:20:43 -0700

    set up ancestor graph using initial data

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,4 @@
+from graph import Graph
 
 def earliest_ancestor(ancestors, starting_node):
 
@@ -20,6 +21,32 @@ def earliest_ancestor(ancestors, starting_node):
     # # count generations
     # current_generation = 0
 
-    # add nodes to graph. Child nodes point to parent nodes.
+    # create a graph to represent the data
+    family_tree = Graph()
 
-    pass
+    # obtain a list of all nodes used in the graph
+    known_nodes = set()
+    
+    for parent_child_pair in ancestors:
+        parent, child = parent_child_pair
+
+        if parent not in known_nodes:
+            known_nodes.add(parent)
+        
+        if child not in known_nodes:
+            known_nodes.add(child)
+
+    # add each node to the graph
+    for node in known_nodes:
+        family_tree.add_vertex(node)
+
+    # add edges to graph. Child nodes point to parent nodes.
+    for parent_child_pair in ancestors:
+        parent, child = parent_child_pair
+        family_tree.add_edge(child, parent)
+
+    print(known_nodes)
+
+test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+
+print(earliest_ancestor(test_ancestors, 1))
