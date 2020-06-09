1634cfc507655af4f24266a5b238b65bc277e7b6
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-06-09 09:10:59 -0700

    begin ancestor problem

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,25 @@
 
 def earliest_ancestor(ancestors, starting_node):
+
+    # # add all ancesters to a dictionary
+    # direct_parents = dict()
+
+    # known_parents = set()
+
+    # for parent_child_pair in ancestors:
+    #     parent, child = parent_child_pair
+    #     direct_parents[child] = parent
+
+    #     # find any parents of starting node
+    #     if child == starting_node:
+    #         known_parents.add(parent)
+
+    # print("all parents", direct_parents)
+    # print("known parents", known_parents)
+
+    # # count generations
+    # current_generation = 0
+
+    # add nodes to graph. Child nodes point to parent nodes.
+
     pass
